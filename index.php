a643c918eaa8c3f02f536a48471a97bb658ac9d1
Rodrigo Molina <rmolina.m@icloud.com>
2025-03-30 16:53:34 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta property="og:title" content="MUSICA FITNESS">
+    <meta name="description" content="Música Fitness para Instructores, cuadrada en 32 tiempos, ideal para tus clases dirigidas,Step, Aerobica, Aerobox, Latin, Ediciones especiales, etc. "/>
+    <meta property="og:image" content="common/LOG.jpg">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700%7CRoboto:400,700%7CKarma:400,600%7CLibre+Baskerville"
+          rel="stylesheet">
+
+    <!-- Font Icons -->
+    <link rel="stylesheet" href="fonts/css/fontawesome-all.min.css">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="plugin-frameworks/bootstrap-grid.min.css">
+
+	<!-- Jquery Magnific for Image Pop up -->
+	<link rel="stylesheet" href="plugin-frameworks/magnific-popup.css">
+	
+    <!-- Styles -->
+    <link rel="stylesheet" href="common/styles.css">
+<META HTTP-EQUIV="REFRESH" CONTENT="1;URL=https://musica.fitchile.cloud/streamz/public/">
+</head>
+<body>
+
+
+
+<div class="loader-container" id="preloader"><div class="progress float shadow"><div class="progress__item"></div></div></div>
+
+
+<header>
+    
+
+
+</body>
+</html>
+
